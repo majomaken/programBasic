dba8390c0baca94e5b056f00d8f9b0a9bcc77501
majomaken <majo.maken@gmail.com>
2018-12-10 15:48:06 -0500

    Imagen tile con canvas lista teniendo a Image como nuestro objeto

--- villaPlatzi/main.js
+++ villaPlatzi/main.js
@@ -1,10 +1,31 @@
-let z = aleatorio(10, 20);
-document.write(z);
+let vp = document.getElementById("villaplatzi");
+let paper = vp.getContext("2d");
+let map = "tile.png";
+
+let image = new Image();
+image.src = map;
+image.addEventListener("load", draw);
+
+function draw()
+{
+  paper.drawImage(image, 0, 0);
+};
+
+
+
+let z;
+// document.write(z);
+
+for(var i=0; i<20; i++)
+{
+  //-5, 5
+  z = aleatorio(10, 20);
+  // document.write(z + ", ");
+}
 
 function aleatorio(min, maxi)
 {
   let result;
-
   result = Math.floor(Math.random() * (maxi - min + 1)) + min;
   return result;
 };

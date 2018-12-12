301b543e839b39258c79a4e8af268b8a1274812a
majomaken <majo.maken@gmail.com>
2018-12-12 18:11:04 -0500

    run

---
+++ fizzbuzz/fizz.js
@@ -0,0 +1,18 @@
+let number = 100;
+
+for(let i = 1; i <= 100; i++)
+{
+  if (i % 3 == 0)
+  {
+    document.write("Fizz");
+  }
+  else if (i % 5 == 0)
+  {
+    document.write("Buzz");
+  }
+  else
+  {
+    document.write(i);
+  }
+  document.write("<br />");
+}
